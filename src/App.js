06c14f308efc03073ac1a7e2a376276cc1a79067
Jack Anderson <whotfisjax@gmail.com>
2024-08-13 23:53:20 -0400

    v0

--- src/App.js
+++ src/App.js
@@ -42,11 +42,11 @@ function App() {
           alt="Purge"
         />
       </div>
-      <div className='absolute top-5 text-center text-white text-[10px] md:text-base'>
+      <div className='absolute top-5 text-center text-[#FF0000] text-[10px] md:text-base'>
         CA: XXXXXXXXXXXXXX
       </div>
       <div className='absolute bottom-5 right-5 flex justify-center items-center z-10'>
-        <a href="https://x.com/" className=''>
+        <a href="https://x.com/solanapurge" className=''>
           <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" className='size-10 md:size-12 md:hover:scale-105 transition ease-in-out duration-150' fill="#FF0000" viewBox="0 0 50 50">
             <path d="M 6.9199219 6 L 21.136719 26.726562 L 6.2285156 44 L 9.40625 44 L 22.544922 28.777344 L 32.986328 44 L 43 44 L 28.123047 22.3125 L 42.203125 6 L 39.027344 6 L 26.716797 20.261719 L 16.933594 6 L 6.9199219 6 z"></path>
           </svg>
